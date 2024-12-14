49654e968db50e693eb5b8bccc56940cff6c65eb
idakkr <ida.kriikkula1@gmail.com>
2024-12-14 15:33:45 +0200

    Update App.tsx

--- login/App.tsx
+++ login/App.tsx
@@ -1,29 +1,65 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 import LoginForm from './LoginForm'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Asetetaan cookie 
+  const setCookie = (name: string, value: string, days: number) => {
+    const expires = new Date(Date.now() + days * 864e5).toUTCString();
+    document.cookie = `${name}=${encodeURIComponent(value)}; expires=${expires}; path=/`; 
+  }; 
 
-  return (
-    <>
-      <div>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+  // Luetaan cookie
+  const getCookie = (name: string): number | 0 => {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) {
+      const num = parseInt(decodeURIComponent(parts.pop()?.split(';').shift() || ''), 10); 
+      return num;
+    }
+    return 0;
+  };
+
+  const [count, setCount] = useState(getCookie(`Count`) | 0 )
+
+  const handleClick = useCallback<React.MouseEventHandler<HTMLButtonElement>>((event) => {
+    console.log('Toiminto*(X, Y): (' + event.pageX + ', ' + + event.pageY + ')');
+    setCount((count) => count + 1);
+    setCookie('Count', "" + count, 1);
+  }, [setCookie]);
+
+  <div className="card">
+        <button onClick={(handleClick)}>
           count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+
+
+return (
+  <>
+    <div>
+    </div>
+    <h1>Vite + React</h1>
+    <div className="card">
+      <button onClick={() => setCount((count) => count + 1)}>
+        Laskurin arvo on {count}
+      </button>
+      <p>
+        Edit <code>src/App.tsx</code> and save to test HMR
+      </p>
+      <p>
         <LoginForm />
       </p>
-    </>
-  )
+    </div>
+    <p className="read-the-docs">
+      Click on the Vite and React logos to learn more
+    </p>
+  </>
+)
 }
 
 export default App
+
